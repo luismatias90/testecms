3ee1c73102e76c78e95c9119d1a690a78d746bfa
Jackson Teixeira <jackson.s.teixeira@gmail.com>
2015-11-30 12:37:07 -0200

    Jersey

--- assets/js/cupom.js
+++ assets/js/cupom.js
@@ -10,6 +10,7 @@
 	}
 
 	ref = $.urlParam('ref');
+
 	//tratamento de ref indicação enviados incorretamente
 	if(ref && ref.indexOf("contabilizei.com.br") > -1){
 		var results = new RegExp('[\\?&]ref=([^&#]*)').exec(ref);
@@ -29,11 +30,9 @@
 	}
 
 	if (ref != null) {
-		
+
 		$(".carregando-cupom").removeClass("hide");
-		
-		addGapiCallBack(function() {
-			
+
 			removeCupom = function() {
 				$.removeCookie('ctblz-ref', {
 					path : '/'
@@ -42,18 +41,59 @@
 				$('#indicacao-msg span').html('');
 				$('#indicacao-msg, #alert-cupom').hide();
 			}
-			
-			
+
+    $.ajax({
+      method : "GET",
+      url : baseCadastro + "/rest/public/importacao/validarindicacao/"+ref,
+    }).done(function(resp) {
+
+				if (resp) {
+					//ARMAZENA COOKIE APENAS SE INDICAÇÃO FOR VÁLIDA
+					$.cookie('ctblz-ref', ref, {
+						expires : 7,
+						path : '/'
+					});
+
+					$(".carregando-cupom").addClass("hide");
+					var c = $('#alert-cupom')
+					c.find('div.img-indicacao img').attr('src', resp.imagem)
+					c.find('div.text-indicacao').html(resp.mensagem);
+					$('.nome-indicador').html(resp.nome);
+					var fnome = resp.nome;
+					$('.fnome-indicador').html(fnome.split(" ")[0]);
+					//c.parent().removeClass("hide");
+					c.parent().slideDown();
+
+					$("html, body").animate({ scrollTop: 0 }, 0);
+				} else {
+					removeCupom();
+					$(".carregando-cupom").html("<span style=\"font-size: 18px\"> <strong>Código inválido. Por favor, verifique e recarregue a página.</strong></span>");
+				}
+
+    });
+
+/*
+		addGapiCallBack(function() {
+
+			removeCupom = function() {
+				$.removeCookie('ctblz-ref', {
+					path : '/'
+				});
+				$('#indicacao').val('');
+				$('#indicacao-msg span').html('');
+				$('#indicacao-msg, #alert-cupom').hide();
+			}
+
 			gapi.client.contabilizei.cupom.valida({
 				'id' : ref
 			}).execute(
 					function(resp) {
-						
+
 						if (resp.id == "-1" || !resp.ativo) {
 							removeCupom();
 							$(".carregando-cupom").html("<span style=\"font-size: 18px\"> <strong>Código inválido. Por favor, verifique e recarregue a página.</strong></span>");
 						} else {
-							//ARMAZENA COOKIE APENAS SE INDICAÇÃO FOR VÁLIDA	
+							//ARMAZENA COOKIE APENAS SE INDICAÇÃO FOR VÁLIDA
 							$.cookie('ctblz-ref', ref, {
 								expires : 7,
 								path : '/'
@@ -68,12 +108,14 @@
 							$('.fnome-indicador').html(fnome.split(" ")[0]);
 							//c.parent().removeClass("hide");
 							c.parent().slideDown();
-							
+
 							$("html, body").animate({ scrollTop: 0 }, 0);
 						}
 
 					});
 		})
+*/
+
 
 	}
 
--- parametros.js
+++ parametros.js
@@ -1,4 +1,28 @@
+var ambiente = "des"
 
-var baseCadastro="https://cadastro.contabilizei.com.br";
-var baseApp="https://appservices.contabilizei.com.br";
-var baseUrl="https://www.contabilizei.com.br";
+// ---------------------------
+// Desenvolvimento
+// ---------------------------
+if (ambiente == "des") {
+  var baseCadastro = "http://localhost:9090";
+  var baseApp = "http://localhost:9999";
+  var baseUrl = "http://localhost:8080";
+}
+
+// ---------------------------
+// Homologação
+// ---------------------------
+if (ambiente == "hom") {
+  var baseCadastro = "https://contabilizei-precadastro-teste.appspot.com";
+  var baseApp = "https://contabilizei-teste.appspot.com";
+  var baseUrl = "https://www.contabilizei.com.br";
+}
+
+// ---------------------------
+// Produção
+// ---------------------------
+if (ambiente == "prod") {
+  var baseCadastro = "https://cadastro.contabilizei.com.br";
+  var baseApp = "https://appservices.contabilizei.com.br";
+  var baseUrl = "https://www.contabilizei.com.br";
+}
